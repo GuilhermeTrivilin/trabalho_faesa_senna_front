e6b7010886acc8ca1e3367ff7927972cf8917514
GuilhermeTrivilin <trivilin.dev@hotmail.com>
2020-12-06 16:16:58 -0300

    segundo commit

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,4 @@
 import { post, get } from './http'
 
-export const login = async ({ email, password }) => await get(`login?email=${email}&password=${password}`)
-// export const login = async ({ email, password }) => await post('login', { email, password })
+// export const login = async ({ email, password }) => await get(`login?email=${email}&password=${password}`)
+export const login = async ({ email, password }) => await post('login', { email, password })
